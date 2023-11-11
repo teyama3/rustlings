030dc5aef1eb79bf04b3ca9b7e5e52396294955b
teyama3 <soldiers0411@gmail.com>
2023-11-11 08:00:30 +0000

    primitive_types3.rs clear

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
